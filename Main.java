2e3038c4a03a871a7c84a602032d888dc9247e7f
j.pikin <pikin@bk.ru>
2023-07-10 16:43:25 +0500

    main and linkedlist add

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] arsg){
+
+        LinkedList list = new LinkedList();
+
+        list.add(5);
+        list.add(2);
+        list.add(9);
+        list.add(0);
+        list.add(-4);
+
+        list.print();
+    }
+
+}
